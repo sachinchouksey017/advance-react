98c573f95aa1c2fb13ab4a183ab8eea1d5d3bc67
sachinchouksey017 <sachin.chouksey@zettalabz.com>
2021-06-13 15:24:01 +0530

    Added use memo hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import ButtonClick from "./components/HOC/ButtonClick/ButtonClick";
 import HoverCount from "./components/HOC/HoverCount/HoverCount";
+import WithUseMemo from "./components/use-memo/WithUseMemo";
 
 function App() {
   return (
     <div className="App">
-      <ButtonClick />
-      <HoverCount />
+      {/* for Hooks */}
+      {/* <ButtonClick />
+      <HoverCount /> */}
+
+      {/* for UseMemo */}
+      <WithUseMemo/>
     </div>
   );
 }
