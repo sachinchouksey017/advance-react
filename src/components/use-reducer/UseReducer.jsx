e2236d2d41237da6aaa30b5913ca18349f721358
sachinchouksey017 <sachin.chouksey@zettalabz.com>
2021-06-22 12:34:20 +0530

    Used use reducer instead of use state

--- src/components/use-reducer/UseReducer.jsx
+++ src/components/use-reducer/UseReducer.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react"
-
+import { useReducer, useState } from "react"
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement'
+}
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.INCREMENT:
+            return { count: state.count + 1 }
+        case ACTIONS.DECREMENT:
+            return { count: state.count - 1 }
+        default:
+            return state
+    }
+}
 const UseReducer = () => {
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
     const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count + 1)
+        // setCount(count + 1)
+        dispatch({ type: ACTIONS.INCREMENT })
     }
     const decrement = () => {
-        setCount(count - 1)
+        // setCount(count - 1)
+        dispatch({ type: ACTIONS.DECREMENT })
+
     }
     return (
         <div>
             <div>Use Reducer Example</div>
             <div>
                 <button onClick={decrement}>decrement</button>
-                count is {count}
+                count is {state.count}
                 <button onClick={increment}>increment</button>
             </div>
 
