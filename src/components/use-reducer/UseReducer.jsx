88bafd6c507dafa5b4603ed6632a2aab1c2dd518
sachinchouksey017 <sachin.chouksey@zettalabz.com>
2021-06-22 12:03:51 +0530

    Added increment decreament

--- src/components/use-reducer/UseReducer.jsx
+++ src/components/use-reducer/UseReducer.jsx
@@ -3,12 +3,21 @@ import { useState } from "react"
 const UseReducer = () => {
     const [count, setCount] = useState(0);
 
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
     return (
         <div>
             <div>Use Reducer Example</div>
             <div>
+                <button onClick={decrement}>decrement</button>
                 count is {count}
+                <button onClick={increment}>increment</button>
             </div>
+
         </div>
     )
 }
