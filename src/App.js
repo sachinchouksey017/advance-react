de0651057259b4c4501c5aa4bb11eb25a617a79e
sachinchouksey017 <sachin.chouksey@zettalabz.com>
2021-06-27 17:04:13 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,52 @@ import { SWRConfig } from 'swr'
 
 import UseCallback from "./components/use-callback/useCallback";
 import WithUseMemo from "./components/use-memo/WithUseMemo";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 function App() {
   return (
-    <div className="App">
-      <ButtonClick />
-      <HoverCount />
-      <SWRConfig value={{ fetcher }}>
-        <SWR />
-      </SWRConfig>
-
-      <UseCallback />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/swr">swr</Link>
+            </li>
+            <li>
+              <Link to="/usecallback">Use callback</Link>
+            </li>
+            <li>
+              <Link to="/usememo">Use memo</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <ButtonClick />
+            <HoverCount />
+          </Route>
+          <Route path="/swr">
+            <SWRConfig value={{ fetcher }}>
+              <SWR />
+            </SWRConfig>
+          </Route>
+          <Route path="/usecallback">
+            <UseCallback />
+          </Route>
+          <Route path="/usememo">
+            <WithUseMemo />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 export default App;
