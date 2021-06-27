7290040c635d840fe1f7dd0ba43f9b1c76793117
sachinchouksey017 <sachin.chouksey@zettalabz.com>
2021-06-27 17:12:28 +0530

    Added Lazy loading and suspence

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import ButtonClick from "./components/HOC/ButtonClick/ButtonClick";
-import HoverCount from "./components/HOC/HoverCount/HoverCount";
-import SWR from './components/useSWR/swr'
-import { SWRConfig } from 'swr'
 
-import UseCallback from "./components/use-callback/useCallback";
-import WithUseMemo from "./components/use-memo/WithUseMemo";
+// import ButtonClick from "./components/HOC/ButtonClick/ButtonClick";
+// import HoverCount from "./components/HOC/HoverCount/HoverCount";
+// import SWR from './components/useSWR/swr'
+import { SWRConfig } from 'swr'
+// import UseCallback from "./components/use-callback/useCallback";
+// import WithUseMemo from "./components/use-memo/WithUseMemo";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import React, { Suspense } from "react";
+const WithUseMemo = React.lazy(() => import("./components/use-memo/WithUseMemo"))
+const UseCallback = React.lazy(() => import("./components/use-callback/useCallback"))
+const SWR = React.lazy(() => import("./components/useSWR/swr"))
+const ButtonClick = React.lazy(() => import("./components/HOC/ButtonClick/ButtonClick"))
+const HoverCount = React.lazy(() => import("./components/HOC/HoverCount/HoverCount"))
+
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 function App() {
   return (
@@ -34,19 +42,27 @@ function App() {
         </nav>
         <Switch>
           <Route exact path="/">
-            <ButtonClick />
-            <HoverCount />
+            <Suspense fallback={<div>Loading...</div>}>
+              <ButtonClick />
+              <HoverCount />
+            </Suspense>
           </Route>
           <Route path="/swr">
-            <SWRConfig value={{ fetcher }}>
-              <SWR />
-            </SWRConfig>
+            <Suspense fallback={<div>Loading...</div>}>
+              <SWRConfig value={{ fetcher }}>
+                <SWR />
+              </SWRConfig>
+            </Suspense>
           </Route>
           <Route path="/usecallback">
-            <UseCallback />
+            <Suspense fallback={<div>Loading...</div>}>
+              <UseCallback />
+            </Suspense>
           </Route>
           <Route path="/usememo">
-            <WithUseMemo />
+            <Suspense fallback={<div>Loading...</div>}>
+              <WithUseMemo />
+            </Suspense>
           </Route>
         </Switch>
       </div>
